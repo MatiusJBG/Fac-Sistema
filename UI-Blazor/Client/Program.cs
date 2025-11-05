88af4ba022d7b5f6e8459e9f321bdd41fe8b0cf9
MatiusJBG <benjaaigaje01@gmail.com>
2025-11-04 22:22:04 -0500

    Por fiiiiiin

--- UI-Blazor/Client/Program.cs
+++ UI-Blazor/Client/Program.cs
@@ -8,8 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<AuthApiService>();
-
-// Use a single HttpClient that targets the Server API (HTTPS port defined in Server launchSettings)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7161/") });
 
 await builder.Build().RunAsync();
